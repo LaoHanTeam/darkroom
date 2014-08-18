ec2afe94a602eb56efa2f8ff21afed0c34528329
ledkk <mailforledkk@126.com>
2014-08-18 17:27:45 +0800

    修改日志相关

--- src/main/java/org/shareGame/Boot.java
+++ src/main/java/org/shareGame/Boot.java
@@ -3,17 +3,21 @@
  */
 package org.shareGame;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * @author ledkk
  *
  */
 public class Boot {
-
+	private static Log logger = LogFactory.getLog(Boot.class);
+		
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
 	}
 	
 
